d2c232d8db9486011131a897ff749c92c7ff0a47
Natanael <natanaelsantosbr@gmail.com>
2020-11-04 21:28:26 -0300

    Configurar SearchView

--- app/src/main/java/br/natanael/android/youtubeapp/activity/MainActivity.java
+++ app/src/main/java/br/natanael/android/youtubeapp/activity/MainActivity.java
@@ -6,12 +6,16 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerVideo;
     private List<Video> videos = new ArrayList<>();
     private AdapterVideo adapterVideo;
+    private MaterialSearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerVideo = findViewById(R.id.recyclerVideo);
+        searchView = findViewById(R.id.searchView);
 
 
         //Configurar toolbar
@@ -45,6 +51,30 @@ public class MainActivity extends AppCompatActivity {
         recyclerVideo.setHasFixedSize(true);
         recyclerVideo.setLayoutManager(new LinearLayoutManager(this));
         recyclerVideo.setAdapter(adapterVideo);
+
+        //configura metodos para SearchView
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+
+            }
+        });
     }
 
     private void recuperarVideos() {
@@ -55,5 +85,17 @@ public class MainActivity extends AppCompatActivity {
         Video video2 = new Video();
         video2.setTitulo("Video 2 muito interessante");
         videos.add(video2);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        MenuItem item = menu.findItem(R.id.menu_search);
+        searchView.setMenuItem(item);
+
+        return true;
+
     }
 }
