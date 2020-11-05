654085e995ba5c3db034a0d5685a6b736124bafc
Natanael <natanaelsantosbr@gmail.com>
2020-11-04 21:42:44 -0300

    Configurar Retrofit

---
+++ app/src/main/java/br/natanael/android/youtubeapp/helper/RetrofitConfig.java
@@ -0,0 +1,15 @@
+package br.natanael.android.youtubeapp.helper;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitConfig {
+    private Retrofit retrofit;
+
+    public static Retrofit getRetrofit() {
+        return new Retrofit.Builder()
+                .baseUrl(YoutubeConfig.URL_BASE)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+}
---
+++ app/src/main/java/br/natanael/android/youtubeapp/helper/YoutubeConfig.java
@@ -0,0 +1,8 @@
+package br.natanael.android.youtubeapp.helper;
+
+public class YoutubeConfig {
+
+    public static String CANAL_ID = "";
+    public static String CHAVE_YOUTUBE_API = "AIzaSyBf7K-Eqt1QYt1VXQCgaM0xUYST0dk_Qas";
+    public static String URL_BASE = "";
+}
