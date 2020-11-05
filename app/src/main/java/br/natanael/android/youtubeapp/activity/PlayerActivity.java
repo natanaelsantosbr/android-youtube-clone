41014b461511947c0fdb1e111275564b07cc04e8
Natanael <natanaelsantosbr@gmail.com>
2020-11-04 23:29:51 -0300

    Exibir video

--- app/src/main/java/br/natanael/android/youtubeapp/activity/PlayerActivity.java
+++ app/src/main/java/br/natanael/android/youtubeapp/activity/PlayerActivity.java
@@ -4,13 +4,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import br.natanael.android.youtubeapp.R;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
-public class PlayerActivity extends AppCompatActivity {
+import br.natanael.android.youtubeapp.R;
+import br.natanael.android.youtubeapp.helper.YoutubeConfig;
+
+public class PlayerActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+    public YouTubePlayerView playerVideo;
+    public String idVideo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
+        playerVideo = findViewById(R.id.playerVideo);
+
+        Bundle bundle = getIntent().getExtras();
+
+        if(bundle != null)
+        {
+            idVideo = bundle.getString("idVideo");
+
+            playerVideo.initialize(YoutubeConfig.CHAVE_YOUTUBE_API, this);
+
+
+        }
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        youTubePlayer.setFullscreen(true);
+        youTubePlayer.setShowFullscreenButton(false);
+        youTubePlayer.loadVideo(idVideo);
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
     }
 }
